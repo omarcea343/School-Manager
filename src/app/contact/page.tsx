41b9f8c5b857b3111e5edd81639750aaac86b617
Omar Zuñiga <3613625+omarcea343@users.noreply.github.com>
2023-11-20 03:55:59 +0000

    Added 3 pages

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,30 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+        <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+          Contact <span className="text-red-500">us</span>
+        </h1>
+        <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
+          School Manager comes with a free tier so you can start using it now and then upgrade to a premium plan.
+        </p>
+
+        <Link
+          className={buttonVariants({
+            size: "lg",
+            className: "mt-5"
+          })}
+          href="/dashboard"
+          target="_blank"
+        >
+          See More <ArrowRight className="ml-2 h-5 w-5" />
+        </Link>
+      </MaxWidthWrapper>
+    </>
+  );
+}
